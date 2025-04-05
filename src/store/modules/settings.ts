7d5c7ab35ffbddb44304ca93269461cfa3a77240
dftre <3066417822@qq.com>
2025-04-05 16:44:54 +0800

    优化初始化设置逻辑，使用空值合并运算符简化代码

--- src/store/modules/settings.ts
+++ src/store/modules/settings.ts
@@ -25,41 +25,15 @@ const useSettingsStore = defineStore('settings', {
     async initSetting() {
       if (this.inited) return
       const config = await initDbSetting()
-      this.theme = storageSetting.theme || config.theme || theme
-      this.sideTheme = storageSetting.sideTheme || config.sideTheme || sideTheme
-      // 顶部导航
-      if (storageSetting.topNav == undefined) {
-        this.topNav = config.topNav == undefined ? topNav : config.topNav
-      } else {
-        this.topNav = storageSetting.topNav
-      }
-      // tab栏
-      if (storageSetting.tagsView == undefined) {
-        this.tagsView = config.tagsView == undefined ? tagsView : config.tagsView
-      } else {
-        this.tagsView = storageSetting.tagsView
-      }
-      // 固定顶部栏
-      if (storageSetting.fixedHeader == undefined) {
-        this.fixedHeader = config.fixedHeader == undefined ? fixedHeader : config.fixedHeader
-      } else {
-        this.fixedHeader = storageSetting.fixedHeader
-      }
-      // 显示logo
-      if (storageSetting.sidebarLogo == undefined) {
-        this.sidebarLogo = config.sidebarLogo == undefined ? sidebarLogo : config.sidebarLogo
-      } else {
-        this.sidebarLogo = storageSetting.sidebarLogo
-      }
-      // 动态标题
-      if (storageSetting.dynamicTitle == undefined) {
-        this.dynamicTitle = config.dynamicTitle == undefined ? dynamicTitle : config.dynamicTitle
-      } else {
-        this.dynamicTitle = storageSetting.dynamicTitle
-      }
+      this.theme = storageSetting.theme ?? config.theme ?? theme
+      this.sideTheme = storageSetting.sideTheme ?? config.sideTheme ?? sideTheme
+      this.topNav = storageSetting.topNav ?? config.topNav ?? topNav
+      this.tagsView = storageSetting.tagsView ?? config.tagsView ?? tagsView
+      this.fixedHeader = storageSetting.fixedHeader ?? config.fixedHeader ?? fixedHeader
+      this.sidebarLogo = storageSetting.sidebarLogo ?? config.sidebarLogo ?? sidebarLogo
+      this.dynamicTitle = storageSetting.dynamicTitle ?? config.dynamicTitle ?? dynamicTitle
       this.inited = true
       handleThemeStyle(this.theme)
-      console.log('initSetting', storageSetting, config, this.fixedHeader)
     },
     // 修改布局设置
     changeSetting(data: { key: keyof typeof storageSetting, value: any }) {
