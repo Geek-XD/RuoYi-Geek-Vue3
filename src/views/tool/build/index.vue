a75185c83c22ead6d496719ef30775e13712a272
dftre <3066417822@qq.com>
2025-05-04 18:02:42 +0800

    更新依赖，修改动态表单界面显示

--- src/views/tool/build/index.vue
+++ src/views/tool/build/index.vue
@@ -1,18 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const vfdRef = ref(null);
+</script>
 <template>
     <div class="app-container">
-        <el-button type="primary" @click="dialogVisible = true">表单构建器</el-button>
-        <el-dialog v-model="dialogVisible" width="100%" fullscreen>
-            <v-form-designer ref="vfdRef" style="margin-left: 0;"></v-form-designer>
-        </el-dialog>
+        <v-form-designer ref="vfdRef" style="margin-left: 0;"></v-form-designer>
     </div>
 </template>
-
-<script setup>
-import { ref } from 'vue';
-
-const vfdRef = ref(null)
-const dialogVisible = ref(false)
-</script>
-
-<style lang="scss">
-</style>
+<style lang="scss"></style>
