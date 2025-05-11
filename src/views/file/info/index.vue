1856fc60c553dc879b1efd678ad8c96cbbc8a023
Dftre <3066417822@qq.com>
2025-05-11 23:44:11 +0800

    删除file无用的代码

--- src/views/file/info/index.vue
+++ src/views/file/info/index.vue
@@ -1,14 +1,8 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="100px">
       <el-form-item label="原始文件名" prop="fileName">
         <el-input v-model="queryParams.fileName" placeholder="请输入原始文件名" clearable @keyup.enter="handleQuery" />
-      </el-form-item>
-      <el-form-item label="文件大小" prop="fileSize">
-        <el-input v-model="queryParams.fileSize" placeholder="请输入文件大小" clearable @keyup.enter="handleQuery" />
-      </el-form-item>
-      <el-form-item label="文件MD5" prop="md5">
-        <el-input v-model="queryParams.md5" placeholder="请输入文件MD5" clearable @keyup.enter="handleQuery" />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -17,13 +11,6 @@
     </el-form>
 
     <el-row :gutter="10" class="mb8">
-      <el-col :span="1.5">
-        <el-button type="primary" plain icon="Plus" @click="handleAdd" v-hasPermi="['file:info:add']">新增</el-button>
-      </el-col>
-      <el-col :span="1.5">
-        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
-          v-hasPermi="['file:info:edit']">修改</el-button>
-      </el-col>
       <el-col :span="1.5">
         <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
           v-hasPermi="['file:info:remove']">删除</el-button>
@@ -48,7 +35,6 @@
       <el-table-column label="文件类型/后缀" align="center" prop="fileType" />
       <el-table-column label="文件大小" align="center" prop="fileSize" />
       <el-table-column label="文件MD5" align="center" prop="md5" />
-      <el-table-column label="备注" align="center" prop="remark" />
       <el-table-column label="预览" align="center">
         <template #default="scope">
           <template v-if="isImage(scope.row.fileType)">
@@ -63,8 +49,6 @@
       </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
-            v-hasPermi="['file:info:edit']">修改</el-button>
           <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
             v-hasPermi="['file:info:remove']">删除</el-button>
           <el-button link type="primary" icon="Download" @click="handleDownload(scope.row)">下载</el-button>
@@ -74,48 +58,6 @@
 
     <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum"
       v-model:limit="queryParams.pageSize" @pagination="getList" />
-
-    <!-- 添加或修改文件信息对话框 -->
-    <el-dialog :title="title" v-model="open" width="500px" append-to-body>
-      <el-form ref="infoRef" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="原始文件名" prop="fileName">
-          <el-input v-model="form.fileName" placeholder="请输入原始文件名" />
-        </el-form-item>
-        <el-form-item label="统一逻辑路径" prop="filePath">
-          <el-input v-model="form.filePath" type="textarea" placeholder="请输入内容" />
-        </el-form-item>
-        <el-form-item label="存储类型" prop="storageType">
-          <el-select v-model="form.storageType" multiple filterable remote reserve-keyword remote-show-suffix
-            placeholder="请选择存储类型" :remote-method="remoteMethodStorageType" :loading="loadingStorageType">
-            <el-option v-for="item in optionsStorageType" :key="item.value" :label="item.label" :value="item.value" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="文件类型/后缀" prop="fileType">
-          <el-select v-model="form.fileType" multiple filterable remote reserve-keyword remote-show-suffix
-            placeholder="请选择文件类型/后缀" :remote-method="remoteMethodFileType" :loading="loadingFileType">
-            <el-option v-for="item in optionsFileType" :key="item.value" :label="item.label" :value="item.value" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="文件大小" prop="fileSize">
-          <el-input v-model="form.fileSize" placeholder="请输入文件大小" />
-        </el-form-item>
-        <el-form-item label="文件MD5" prop="md5">
-          <el-input v-model="form.md5" placeholder="请输入文件MD5" />
-        </el-form-item>
-        <el-form-item label="备注" prop="remark">
-          <el-input v-model="form.remark" placeholder="请输入备注" />
-        </el-form-item>
-        <el-form-item label="删除标志" prop="delFlag">
-          <el-input v-model="form.delFlag" placeholder="请输入删除标志" />
-        </el-form-item>
-      </el-form>
-      <template #footer>
-        <div class="dialog-footer">
-          <el-button type="primary" @click="submitForm">确 定</el-button>
-          <el-button @click="cancel">取 消</el-button>
-        </div>
-      </template>
-    </el-dialog>
 
     <el-dialog v-model="openUploadDialog" title="上传文件" width="500px" append-to-body>
       <el-form :model="uploadForm" label-width="80px">
@@ -131,12 +73,12 @@
           </el-select>
         </el-form-item>
         <el-form-item v-if="uploadForm.uploadType === 'image'" label="图片上传">
-          <ImageUpload :limit="5" :fileSize="10" :isShowTip="true" :uploadImgUrl="getUnifiedUploadUrl"
+          <ImageUpload :limit="5" :fileSize="10" :isShowTip="true" :uploadImgUrl="uploadUrl"
             @update:modelValue="onUploadSuccess" style="width:100%"
             :fileType="['jpg', 'jpeg', 'png', 'gif', 'webp', 'bmp', 'svg']" />
         </el-form-item>
         <el-form-item v-else label="文件上传">
-          <FileUpload :limit="5" :fileSize="10" :isShowTip="true" :uploadFileUrl="getUnifiedUploadUrl"
+          <FileUpload :limit="5" :fileSize="10" :isShowTip="true" :uploadFileUrl="uploadUrl"
             @update:modelValue="onUploadSuccess" style="width:100%"
             :fileType="['doc', 'xls', 'ppt', 'txt', 'pdf', 'zip', 'rar', '7z', 'jpg', 'jpeg', 'png', 'gif', 'webp', 'bmp', 'svg']" />
         </el-form-item>
@@ -146,26 +88,23 @@
 </template>
 
 <script setup name="Info">
-import { listInfo, getInfo, delInfo, addInfo, updateInfo, uploadFileUnified, downloadFileUnified, getClientList } from '@/api/file/info';
-import ImagePreview from "@/components/ImagePreview";
-import ImageUpload from '@/components/ImageUpload';
-import FileUpload from '@/components/FileUpload';
+import { listInfo, delInfo, uploadFileUnified, downloadFileUnified, getClientList } from '@/api/file/info';
+import ImagePreview from "@/components/ImagePreview/index.vue";
+import ImageUpload from '@/components/ImageUpload/index.vue';
+import FileUpload from '@/components/FileUpload/index.vue';
 import { onMounted } from 'vue';
 
 const { proxy } = getCurrentInstance();
 
 const infoList = ref([]);
-const open = ref(false);
 const loading = ref(true);
 const showSearch = ref(true);
 const ids = ref([]);
 const single = ref(true);
 const multiple = ref(true);
 const total = ref(0);
-const title = ref("");
 
 const data = reactive({
-  form: {},
   queryParams: {
     pageNum: 1,
     pageSize: 10,
@@ -175,21 +114,10 @@ const data = reactive({
     fileType: null,
     fileSize: null,
     md5: null,
-  },
-  rules: {
-    fileName: [
-      { required: true, message: "原始文件名不能为空", trigger: "blur" }
-    ],
-    filePath: [
-      { required: true, message: "统一逻辑路径不能为空", trigger: "blur" }
-    ],
-    storageType: [
-      { required: true, message: "存储类型不能为空", trigger: "change" }
-    ],
   }
 });
 
-const { queryParams, form, rules } = toRefs(data);
+const { queryParams } = toRefs(data);
 
 /** 查询文件信息列表 */
 function getList() {
@@ -199,32 +127,6 @@ function getList() {
     total.value = response.total;
     loading.value = false;
   });
-}
-
-// 取消按钮
-function cancel() {
-  open.value = false;
-  reset();
-}
-
-// 表单重置
-function reset() {
-  form.value = {
-    fileId: null,
-    fileName: null,
-    filePath: null,
-    storageType: null,
-    fileType: null,
-    fileSize: null,
-    md5: null,
-    createBy: null,
-    createTime: null,
-    updateBy: null,
-    updateTime: null,
-    remark: null,
-    delFlag: null
-  };
-  proxy.resetForm("infoRef");
 }
 
 /** 搜索按钮操作 */
@@ -246,44 +148,6 @@ function handleSelectionChange(selection) {
   multiple.value = !selection.length;
 }
 
-/** 新增按钮操作 */
-function handleAdd() {
-  reset();
-  open.value = true;
-  title.value = "添加文件信息";
-}
-
-/** 修改按钮操作 */
-function handleUpdate(row) {
-  reset();
-  const _fileId = row.fileId || ids.value
-  getInfo(_fileId).then(response => {
-    form.value = response.data;
-    open.value = true;
-    title.value = "修改文件信息";
-  });
-}
-
-/** 提交按钮 */
-function submitForm() {
-  proxy.$refs["infoRef"].validate(valid => {
-    if (valid) {
-      if (form.value.fileId != null) {
-        updateInfo(form.value).then(response => {
-          proxy.$modal.msgSuccess("修改成功");
-          open.value = false;
-          getList();
-        });
-      } else {
-        addInfo(form.value).then(response => {
-          proxy.$modal.msgSuccess("新增成功");
-          open.value = false;
-          getList();
-        });
-      }
-    }
-  });
-}
 
 /** 删除按钮操作 */
 function handleDelete(row) {
@@ -342,7 +206,7 @@ const clientList = computed(() =>
   )
 );
 // 兼容ImageUpload和FileUpload的上传url属性
-const getUnifiedUploadUrl = computed(() => {
+const uploadUrl = computed(() => {
   if (!uploadForm.clientKey) return '';
   const [storageType, clientName] = uploadForm.clientKey.split(":");
   return `${import.meta.env.VITE_APP_BASE_API}/file/${storageType}/${clientName}/upload`;
