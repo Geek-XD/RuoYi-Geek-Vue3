a1802f64b0d445b882a86532fcd63e26e3882417
dftre <3066417822@qq.com>
2026-02-07 19:13:26 +0800

    优化细节

--- src/components/DictTag/index.vue
+++ src/components/DictTag/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <template v-for="(item, index) in options">
-      <template v-if="values.includes(item.value)">
+      <template v-if="isValueMatch(item.value)">
         <span v-if="item.elTagType == 'default' || item.elTagType == ''" :key="item.value" :index="index"
           :class="item.elTagClass">{{ item.label }}</span>
         <el-tag v-else :disable-transitions="true" :key="item.value + ''" :index="index" :type="item.elTagType"
@@ -14,18 +14,25 @@
 <script setup lang="ts">
 import { computed } from 'vue';
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
+  separator: string;
+  showValue: boolean
   options?: Array<{ label: string; value: string | number; elTagType?: string; elTagClass?: string }>;
   value?: string | number | Array<string | number> | null;
-}>()
+}>(), {
+  separator: ',',
+  showValue: true,
+})
 
 const values = computed(() => {
-  if (props.value !== null && typeof props.value !== 'undefined') {
-    return Array.isArray(props.value) ? props.value : [String(props.value)];
-  } else {
-    return [];
-  }
+  if (props.value === null || typeof props.value === 'undefined' || props.value === '') return []
+  if (typeof props.value === 'number' || typeof props.value === 'boolean') return [props.value]
+  return Array.isArray(props.value) ? props.value.map(item => '' + item) : String(props.value).split(props.separator)
 })
+
+function isValueMatch(itemValue: string | number) {
+  return values.value.some(val => val == itemValue);
+}
 
 </script>
 
